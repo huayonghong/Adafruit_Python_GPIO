828c2b4f896f2ada07f0f0785e107687e12fdf08
Andrey Somov <public.somov@gmail.com>
2015-03-30 10:32:43 +0200

    Fix tests for Mac

--- tests/test_Platform.py
+++ tests/test_Platform.py
@@ -18,7 +18,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 import unittest
-import os
+import sys
 from mock import patch
 
 import Adafruit_GPIO.Platform as Platform
@@ -35,63 +35,65 @@ class TestCreateSystemConfig(unittest.TestCase):
 
     def test_read_proc_info(self):
         p = Platform.read_proc_info()
-        if os.name == 'posix':
+        if sys.platform == 'linux':
             self.assertTrue(len(p) > 10)
         else:
             self.assertTrue(len(p) == 0)
 
     def test_read_proc_info_beaglebone(self):
-        with patch('__builtin__.open') as mock_open:
-            handle = mock_open.return_value.__enter__.return_value
-            content = """
-                processor	: 0
-                model name	: ARMv7 Processor rev 2 (v7l)
-                BogoMIPS	: 298.24
-                Features	: swp half thumb fastmult vfp edsp thumbee neon vfpv3 tls
-                CPU implementer	: 0x41
-                CPU architecture: 7
-                CPU variant	: 0x3
-                CPU part	: 0xc08
-                CPU revision	: 2
+        if sys.platform == 'linux':
+            with patch('__builtin__.open') as mock_open:
+                handle = mock_open.return_value.__enter__.return_value
+                content = """
+                    processor	: 0
+                    model name	: ARMv7 Processor rev 2 (v7l)
+                    BogoMIPS	: 298.24
+                    Features	: swp half thumb fastmult vfp edsp thumbee neon vfpv3 tls
+                    CPU implementer	: 0x41
+                    CPU architecture: 7
+                    CPU variant	: 0x3
+                    CPU part	: 0xc08
+                    CPU revision	: 2
 
-                Hardware	: Generic AM33XX (Flattened Device Tree)
-                Revision	: 0000
-                Serial		: 0000000000000000
-            """
-            handle.read.return_value = content
-            p = Platform.read_proc_info()
-            self.assertEqual(len(p), 12)
-            self.assertEqual(p['model name'], 'ARMv7 Processor rev 2 (v7l)'.lower(), 'The values must be lower case')
-            self.assertEqual(p['hardware'], 'Generic AM33XX (Flattened Device Tree)'.lower(),
-                             'The keys must be lower case')
+                    Hardware	: Generic AM33XX (Flattened Device Tree)
+                    Revision	: 0000
+                    Serial		: 0000000000000000
+                """
+                handle.read.return_value = content
+                p = Platform.read_proc_info()
+                self.assertEqual(len(p), 12)
+                self.assertEqual(p['model name'], 'ARMv7 Processor rev 2 (v7l)'.lower(), 'The values must be lower case')
+                self.assertEqual(p['hardware'], 'Generic AM33XX (Flattened Device Tree)'.lower(),
+                                 'The keys must be lower case')
 
     def test_read_proc_info_intel(self):
-        with patch('__builtin__.open') as mock_open:
-            handle = mock_open.return_value.__enter__.return_value
-            content = """
-                processor	: 0
-                vendor_id	: GenuineIntel
-                cpu family	: 6
-                model		: 42
-                model name	: Intel(R) Core(TM) i3-2310M CPU @ 2.10GHz
-                cpu MHz		: 800.000
-                cache size	: 3072 KB
-                power management:
+        if sys.platform == 'linux':
+            with patch('__builtin__.open') as mock_open:
+                handle = mock_open.return_value.__enter__.return_value
+                content = """
+                    processor	: 0
+                    vendor_id	: GenuineIntel
+                    cpu family	: 6
+                    model		: 42
+                    model name	: Intel(R) Core(TM) i3-2310M CPU @ 2.10GHz
+                    cpu MHz		: 800.000
+                    cache size	: 3072 KB
+                    power management:
 
-                processor	: 1
-                vendor_id	: GenuineIntel
-                cpu family	: 6
-                model		: 42
-                model name	: Intel(R) Core(TM) i3-2310M CPU @ 2.10GHz
-                cpu MHz		: 800.000
-                cache size	: 3072 KB
-                power management:
+                    processor	: 1
+                    vendor_id	: GenuineIntel
+                    cpu family	: 6
+                    model		: 42
+                    model name	: Intel(R) Core(TM) i3-2310M CPU @ 2.10GHz
+                    cpu MHz		: 800.000
+                    cache size	: 3072 KB
+                    power management:
 
-            """
-            handle.read.return_value = content
-            p = Platform.read_proc_info()
-            self.assertEqual(len(p), 8)
-            self.assertEqual(p['power management'], '')
+                """
+                handle.read.return_value = content
+                p = Platform.read_proc_info()
+                self.assertEqual(len(p), 8)
+                self.assertEqual(p['power management'], '')
 
     def test_create_system_config(self):
         config = Platform.create_system_config()
